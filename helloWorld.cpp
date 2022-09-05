1ff3072d257f8eb3664883a7574176c4fd4b7afb
Aaditya <aaditya.telange@gmail.com>
2022-09-06 02:37:30 +0530

    basic addition program

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -6,3 +6,8 @@ int main()
     cout << "Hello World";
     return 0;
 }
+
+// git status
+// git commit -m "Your message"
+// git push
+// git pull (if you have done any changes via upload or any other means)
