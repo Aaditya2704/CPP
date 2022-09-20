8a917d900741ebfa70f20e93ad6cfb5090d5742b
Aaditya <aaditya.telange@gmail.com>
2022-09-20 19:53:23 +0530

    updatedd

--- alphabeticalHollowDiamond.cpp
+++ alphabeticalHollowDiamond.cpp
@@ -5,6 +5,8 @@ int main()
 {
     int a = 6;
 
+    char ch = 65;
+
     for (int i = 1; i <= a; i++)
     {
         for (int k = 1; k <= a - i; k++)
@@ -13,69 +15,21 @@ int main()
         }
         for (int j = 1; j <= i; j++)
         {
-            if (i == 1)
+            if ((j == 1) || (j == i))
             {
-                cout << "A ";
+                cout << ch << " ";
             }
-            if (i == 2)
+            else
             {
-                if (i == 2 && j == 1 || i == 2 && j == i)
-                {
-                    cout << "B ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            if (i == 3)
-            {
-                if (i == 3 && j == 1 || i == 3 && j == i)
-                {
-                    cout << "C ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            if (i == 4)
-            {
-                if (i == 4 && j == 1 || i == 4 && j == i)
-                {
-                    cout << "D ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            if (i == 5)
-            {
-                if (i == 5 && j == 1 || i == 5 && j == i)
-                {
-                    cout << "E ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            if (i == 6)
-            {
-                if (i == 6 && j == 1 || i == 6 && j == i)
-                {
-                    cout << "F ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
+                cout << "  ";
             }
         }
+        ch++;
         cout << endl;
     }
-    for (int i = 1; i <= a - 1; i++)
+    ch--;
+    ch--;
+    for (int i = 1; i < a; i++)
     {
         for (int k = 0; k < i; k++)
         {
@@ -83,66 +37,16 @@ int main()
         }
         for (int j = a - 1; j >= i; j--)
         {
-            if (i == 1)
+            if (j == a - 1 || j == i)
             {
-                if (i == 1 && j == a - 1 || i == 1 && j == i)
-                {
-                    cout << "E ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
+                cout << ch << " ";
             }
-            if (i == 2)
+            else
             {
-                if (i == 2 && j == a - 1 || i == 2 && j == i)
-                {
-                    cout << "D ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            if (i == 3)
-            {
-                if (i == 3 && j == a - 1 || i == 3 && j == i)
-                {
-                    cout << "C ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            if (i == 4)
-            {
-                if (i == 4 && j == a - 1 || i == 4 && j == i)
-                {
-                    cout << "B ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            if (i == 5)
-            {
-                if (i == 5 && j == a - 1 || i == 5 && j == i)
-                {
-                    cout << "A ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            if (i == 6)
-            {
-                continue;
+                cout << "  ";
             }
         }
+        ch--;
         cout << endl;
     }
 }
